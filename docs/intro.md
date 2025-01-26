982f5fc41306605d99fc67d7199de093634bed07
daniel-lujan <daniel.lujan@udea.edu.co>
2025-01-26 18:38:37 -0500

    :tada: Initialize repository

---
+++ docs/intro.md
@@ -0,0 +1,22 @@
+---
+sidebar_position: 1
+---
+
+# Introduction
+
+On this page you will learn how to integrate your Tiendify API into your custom shop website.
+
+## The basics
+
+The first thing you should know is that the API you will be comunicating with is a dedicated server for your shop.
+
+**Advantages of dedicated API server:**
+
+- Secured and isolated data from other Tiendify customers
+- You have dedicated, _physical_ resources only to serve your shop. This ensures high availability, reliance and avoids resource-racing.
+
+:::info
+
+Some of your store's data (such as public products) can be accessed publicly without any authentication like API keys or Public Key. This means the integration is as straightforward as starting to call the API endpoints.
+
+:::
---
+++ docusaurus.config.ts
@@ -0,0 +1,142 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+import type * as OpenApiPlugin from "docusaurus-plugin-openapi-docs";
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: "Tiendify Docs",
+  tagline: "Tiendify API documentation",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://tiendify.com",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "tiendify", // Usually your GitHub org/user name.
+  projectName: "tiendiy", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: "./sidebars.ts",
+          docItemComponent: "@theme/ApiItem",
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ["rss", "atom"],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          // Useful options to enforce blogging best practices
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  plugins: [
+    [
+      "docusaurus-plugin-openapi-docs",
+      {
+        id: "api",
+        docsPluginId: "classic",
+        config: {
+          petstore: {
+            specPath: "test.openapi.json",
+            outputDir: "docs/tiendify",
+            sidebarOptions: {
+              groupPathsBy: "tag",
+              categoryLinkSource: "tag",
+            },
+          } satisfies OpenApiPlugin.Options,
+        },
+      },
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: "img/docusaurus-social-card.jpg",
+    navbar: {
+      title: "Tiendify",
+      logo: {
+        alt: "Tiendify Logo",
+        src: "img/logo.svg",
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "docsSidebar",
+          position: "left",
+          label: "Documentation",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "API Reference",
+              to: "/docs/intro",
+            },
+          ],
+        },
+        {
+          title: "Links",
+          items: [
+            {
+              label: "Github",
+              href: "https://github.com/L2G-Solutions/tiendify-docs",
+            },
+          ],
+        },
+        {
+          title: "Contact",
+          items: [
+            {
+              label: "Email",
+              to: "mailto:tiendify@gmail.com",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Tiendify. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+  themes: ["docusaurus-theme-openapi-docs"],
+};
+
+export default config;
